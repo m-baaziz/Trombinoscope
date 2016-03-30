caf3c2f43ca52beace16486b968c31e5e980e155
m-baaziz <mha.baaziz@gmail.com>
2016-03-30 16:25:40 +0200

    verfication and signals updated

--- js/src/components/SearchBar.react.js
+++ js/src/components/SearchBar.react.js
@@ -4,23 +4,28 @@ import React, {Component} from 'react';
 import Switch from 'react-bootstrap-switch';
 import _ from 'lodash';
 
+const REGEXP = /^[A-Za-z]+$/;
+
 class SearchBar extends Component {
 
 	constructor(props) {
 		super(props);
-		this.setLastName = this.setLastName.bind(this);
-		this.setFirstName = this.setFirstName.bind(this);
+		this.setName = this.setName.bind(this);
 		this.onSubmit = this.onSubmit.bind(this);
 		this.onCheckboxChange = this.onCheckboxChange.bind(this);
 		this.onRadioChange = this.onRadioChange.bind(this);
 		this.onStructureSelect = this.onStructureSelect.bind(this);
 		this.onSubStructureSelect = this.onSubStructureSelect.bind(this);
 		this.messageContent = this.messageContent.bind(this);
-		this.isSubmitDisabled = this.isSubmitDisabled.bind(this);
 		const {firstName, lastName, options, selectedStructure, selectedSecondaryStructure} = this.props;
 		const searchType = _.includes(window.location.search, "struct") ? "structure" : "identity";
 		this.state = {
-			firstName: firstName || "", lastName: lastName || "", options, searchType, 
+			firstName: firstName || "",
+			lastName: lastName || "",
+			firstNameStatus: "pending",
+			lastNameStatus: "pending",
+			options, 
+			searchType,
 			primaryStructure: selectedStructure || null,
 			secondaryStructure: selectedSecondaryStructure || 0};
 	}
@@ -44,12 +49,18 @@ class SearchBar extends Component {
 		searchType == "identity" ? onSubmitIdentity(firstName, lastName) : onSubmitStructures(primaryStructure || structures[0].structure.structId, secondaryStructure);
 	}
 
-	setLastName(e) {
-		this.setState({lastName: e.target.value});
-	}
-
-	setFirstName(e) {
-		this.setState({firstName: e.target.value});
+	setName(key, e) {
+		const {value, parentElement} = e.target;
+		let status = "pending";
+		if (!_.isEmpty(value) && !value.match(REGEXP)) {
+			status = "warning";
+		} else {
+			if (value.length > 1) {
+				status = "ok";
+			}
+		}
+		console.log(status);
+		this.setState({[key]: value, [`${key}Status`]: status});
 	}
 
 	onCheckboxChange(e) {
@@ -71,32 +82,30 @@ class SearchBar extends Component {
 	}
 
 	messageContent() {
-		const {firstName, lastName} = this.state;
+		const {firstNameStatus, lastNameStatus} = this.state;
 		let invalidFields = [];
-		const regExp = /^[A-Za-z]+$/;
-		if (!_.isEmpty(firstName) && !firstName.match(regExp)) {
-			invalidFields.push("Prénom");
-		}
-		if (!_.isEmpty(lastName) && !lastName.match(regExp)) {
-			invalidFields.push("Nom");
-		}
+   	if (firstNameStatus == "warning") {
+      invalidFields.push("Prénom");
+   	}
+   	if (lastNameStatus == "warning") {
+      invalidFields.push("Nom");
+   	}
 		const errors = _.map(invalidFields, (field, index) => {
 			return (
 				<div className="alert alert-danger" key={index} role="alert"><b>Attention !</b> Le {field} ne doit contenir que des lettres</div>
 			)
 		})
-
 		return errors;
-	}
-
-	isSubmitDisabled() {
-		const {firstName, lastName, searchType} = this.state;
-		return (!_.isEmpty(this.messageContent()) || (_.isEmpty(firstName) && _.isEmpty(lastName)) || (!_.isEmpty(firstName) && firstName.length < 2) || (!_.isEmpty(lastName) && lastName.length < 2)) && searchType == "identity";
 	}
 
 	render() {
 		const {firstName, lastName, options, primaryStructure, secondaryStructure, searchType} = this.state;
 		const {structures, subStructures} = this.props;
+
+		const messageContent = this.messageContent();
+
+		const isSubmitDisabled = (!_.isEmpty(messageContent) || (_.isEmpty(firstName) && _.isEmpty(lastName)) || (!_.isEmpty(firstName) && firstName.length < 2) || (!_.isEmpty(lastName) && lastName.length < 2)) && searchType == "identity";
+
 		const buildCheckBox = (name, value) => {
 			return (
 				<div style={{marginLeft: "3px"}} className="checkbox">
@@ -107,16 +116,29 @@ class SearchBar extends Component {
 			);
 		}
 
+		const getInputIconClass = field => {
+			switch(this.state[`${field}Status`]) {
+				case "warning" :
+					return "glyphicon glyphicon-warning-sign form-control-feedback"
+				case "pending" :
+				 	return "glyphicon glyphicon-pencil form-control-feedback"
+				case "ok" :
+					return "glyphicon glyphicon-ok form-control-feedback"
+				default :
+					return "";
+			}
+		}
+
 		const textInputs = (
 			[
 				<div id="div-name" key={1} className="form-group has-feedback">
-					<input type="text" className="form-control" placeholder="Nom" value={lastName} onChange={this.setLastName} />	
-					<span id="icon-name" className="glyphicon glyphicon-pencil form-control-feedback" ariaHidden="true"></span>
+					<input type="text" className="form-control" placeholder="Nom" value={lastName} onChange={this.setName.bind(this, "lastName")} />	
+					<span id="icon-name" className={getInputIconClass("lastName")} ariaHidden="true"></span>
 					<span id="input-name" className="sr-only">(success)</span>
 				</div>,
 				<div id="div-fname" key={2} className="form-group has-feedback">
-					<input type="text" className="form-control" placeholder="Prénom" value={firstName} onChange={this.setFirstName} />		
-					<span className="glyphicon glyphicon-pencil form-control-feedback" ariaHidden="true"></span>  								  						  								
+					<input type="text" className="form-control" placeholder="Prénom" value={firstName} onChange={this.setName.bind(this, "firstName")} />		
+					<span className={getInputIconClass("firstName")} ariaHidden="true"></span>  								  						  								
 					<span id="input-fname" className="sr-only">(success)</span>
 				</div>
 			]
@@ -161,8 +183,8 @@ class SearchBar extends Component {
 					<form className="form-inline" onSubmit={this.onSubmit}>
 						{searchType == "identity" ? textInputs : selectInputs}
 																										
-						<input type="submit" value="Valider" style={{marginLeft: '3px'}} className={`btn ${this.isSubmitDisabled() ? '' : 'btn-success'}`}
-							disabled={this.isSubmitDisabled()}
+						<input type="submit" value="Valider" style={{marginLeft: '3px'}} className={`btn ${isSubmitDisabled ? '' : 'btn-success'}`}
+							disabled={isSubmitDisabled}
 						/>
 						<div className='row'>
 							{buildCheckBox('photo', 'photo')}
@@ -173,7 +195,7 @@ class SearchBar extends Component {
 					</form>
 				</div>
 				<div id="msg">
-					{this.messageContent()}
+					{messageContent}
 				</div>
 			</div>
 		);
