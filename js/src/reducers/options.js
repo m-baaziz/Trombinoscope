e641fcbae03b3ad5db288bcb0af397e412b919bf
m-baaziz <mha.baaziz@gmail.com>
2016-04-07 15:37:53 +0200

    restore node module

--- js/src/reducers/options.js
+++ js/src/reducers/options.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 import {ActionTypes} from '../constants/Constants';
 
-function options(state = ["photo"], action) {
+function options(state = ["photo", "nom"], action) {
 	if (action.type == ActionTypes.SWAP_OPTION) {
 		let newState = state.slice();
 		_.includes(newState, action.option) ? _.pull(newState, action.option) : newState.push(action.option);
